449d8fa0c74b9661507f6aee24b02a219a6ae2e9
neo <stan183183@gmail.com>
2023-02-15 20:19:28 +0800

    订单逻辑

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -7,22 +7,22 @@ import config from '~/config'
 
 // 这边由于后端没有区分测试和正式，姑且都写成一个接口。
 // axios.defaults.baseURL = config[import.meta.env.MODE].baseUrl
-
 // axios.defaults.baseURL = "http://134.122.197.173/9191/manage-api/v1"
 
 //导弹
-axios.defaults.baseURL = "https://wildberries.store/manage-api/v1"
+// axios.defaults.baseURL = "https://wildberries.store/manage-api/v1"
 
 //飞哥
 // axios.defaults.baseURL = "https://wildberries.cool/manage-api/v1"
 
+//海王
+axios.defaults.baseURL = "https://wildberries.plus/manage-api/v1"
+
+
 
 // axios.defaults.baseURL ="https://wildberriesgoods.top/manage-api/v1";
-
 // axios.defaults.baseURL ="https://shoppe.ink/manage-api/v1";
-
 // axios.defaults.baseURL ="http://localhost:9191/manage-api/v1"
-
 
 
 axios.defaults.withCredentials = true
